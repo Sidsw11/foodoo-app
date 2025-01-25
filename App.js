b0f2a038cb3f682cc41d25592975a263add8833c
Siddhant Swain <siddhantswain11@gmail.com>
2025-01-25 18:32:22 +0530

    config driven ui,data from Swiggy for 1 card

--- App.js
+++ App.js
@@ -21,15 +21,107 @@ const HeadingComponent = () => {
     </div>
 )
 };
-const RestroCard = ({restaurantName, cuisine, dishName, ratings, deliverytime}) => {
+const restObj = {
+    "info": {
+      "id": "205745",
+      "name": "Rest Inn",
+      "cloudinaryImageId": "cfrttjkar7i7yh7yeydv",
+      "locality": "Marine Drive Road",
+      "areaName": "Dolamandap Sahi",
+      "costForTwo": "₹300 for two",
+      "cuisines": [
+        "North Indian"
+      ],
+      "avgRating": 3.7,
+      "parentId": "169018",
+      "avgRatingString": "3.7",
+      "totalRatingsString": "454",
+      "sla": {
+        "deliveryTime": 46,
+        "lastMileTravel": 2.5,
+        "serviceability": "SERVICEABLE",
+        "slaString": "45-50 mins",
+        "lastMileTravelString": "2.5 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2025-01-25 23:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "₹50 OFF",
+        "subHeader": "ABOVE ₹199",
+        "discountTag": "FLAT DEAL"
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    }
+};
+const RestroCard = ({restData}) => {
     return(
         <div className='restcard'>
-            <img id="dishpicture"src={dishpic}></img>
+            <img id="dishpicture"src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+restObj.info.cloudinaryImageId}></img>
             <div className='restdetails'>
-                <div><strong>{restaurantName}</strong></div>
-                <div>{dishName},{cuisine}</div>
-                <div>{ratings}</div>
-                <div>{deliverytime}</div>
+                <div><strong>{restObj.info.name}</strong></div>
+                <div>{restObj.info.cuisines.join(",")}</div>
+                <div>{restObj.info.avgRating+ "⭐"}</div>
+                <div>{restObj.info.costForTwo}</div>
+                <div>{restObj.info.sla.slaString}</div>
             </div>
         </div>
     )
@@ -43,9 +135,8 @@ const BodyComponent =() =>{
             </button>
         </div>
         <div className='rest_container'>
-            <RestroCard restaurantName="Hotel Truptee" dishName="Veg pulao" cuisine="Indian" ratings="4.3⭐" deliverytime="20 minutes" />
-            <RestroCard restaurantName="Sri Rama Hotel" dishName="Veg Fried Rice" cuisine="Asian" ratings="4.0⭐" deliverytime="30 minutes"/>
-            <RestroCard/>
+            
+            <RestroCard restData={restObj}/>
             <RestroCard/>
             <RestroCard/>
             <RestroCard/>
