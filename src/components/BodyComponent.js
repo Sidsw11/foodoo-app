52b063d263b4ee04be2033107ba6dddc6af3d4eb
Siddhant Swain <siddhantswain11@gmail.com>
2025-01-29 16:13:43 +0530

    used the live api data,removed mock data from body,combination of both hooks

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,19 +1,22 @@
 import RestroCard from "./RestroCard.js";
 import restList from "../utils/mockData.js";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 const BodyComponent = () => {
-  const [restaurantList, setrestaurantList] = useState(restList);
-  useEffect(()=>{
+  const [restaurantList, setrestaurantList] = useState([]);
+  useEffect(() => {
     fetchData();
-  },[]);
+  }, []);
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.8134554&lng=85.8312359&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.8134554&lng=85.8312359&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
     const json = await data.json();
-    console.log(data);
     console.log(json);
-    
-  }
+    setrestaurantList(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
   return (
     <div className="bodycomp">
       <div>
