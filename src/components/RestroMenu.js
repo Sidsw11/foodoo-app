3c42b5a1df5a4739f6dbae6fb730594d109e9e10
Siddhant Swain <siddhantswain11@gmail.com>
2025-02-04 12:11:52 +0530

    fetched the name, rating and cuisines of a resturant from api

--- src/components/RestroMenu.js
+++ src/components/RestroMenu.js
@@ -1,20 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const RestroMenu = () => {
-  // useEffect(()=>{
-  //     fetchData();
-  // },[])
-  // const fetchData =async ()=>{
-  //     const data =await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.8134554&lng=85.8312359&restaurantId=702417&catalog_qa=undefined&submitAction=ENTER");
+  const [resMenu, setResMenu] = useState([]);
 
-  //     const json= await data.json();
-  //     const restID=json;
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
-  // }
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.8134554&lng=85.8312359&restaurantId=702417&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = await data.json();
+
+    setResMenu(json.data);
+  };
+  console.log(resMenu);
+
   return (
     <div>
-      <h1>Name</h1>
-      <h2>Rating {" . "} Cost for two</h2>
+      <h1>{resMenu?.cards[2]?.card?.card?.info?.name}</h1>
+      <h1>{resMenu?.cards[2]?.card?.card?.info?.avgRating}</h1>
+      <h2>{resMenu?.cards[2]?.card?.card?.info?.cuisines.join(", ")}</h2>
       <div>
         Menu
         <ul>item 1</ul>
