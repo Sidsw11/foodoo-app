96f21f789a5957f614fa07f0bd7c3e41ec468088
Siddhant Swain <siddhantswain11@gmail.com>
2025-02-05 11:11:07 +0530

    Resolved the undefined issue of RestroMenu, added return keyword before shimmer

--- src/components/RestroMenu.js
+++ src/components/RestroMenu.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer.js";
 
 const RestroMenu = () => {
-  const [resMenu, setResMenu] = useState([]);
+  
+  const [resMenu, setResMenu] = useState(null);
 
   useEffect(() => {
     fetchMenu();
@@ -9,19 +11,25 @@ const RestroMenu = () => {
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.8134554&lng=85.8312359&restaurantId=702417&catalog_qa=undefined&submitAction=ENTER"
-    );
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.8134554&lng=85.8312359&restaurantId=702417&catalog_qa=undefined&submitAction=ENTER");
     const json = await data.json();
-
-    setResMenu(json.data);
+    //console.log(json);
+    setResMenu(json.data?.cards[2]?.card?.card?.info);
+    console.log(json.data?.cards[2]?.card?.card?.info)
+    
   };
-  console.log(resMenu);
+  
+  if (resMenu === null) return<Shimmer />;
+  
+  const { name, avgRating, cuisines } = resMenu;
+  console.log("qwe ", cuisines);
 
+ // return <h3>{JSON.stringify(cuisines)}</h3>;
   return (
     <div>
-      <h1>{resMenu?.cards[2]?.card?.card?.info?.name}</h1>
-      <h1>{resMenu?.cards[2]?.card?.card?.info?.avgRating}</h1>
-      <h2>{resMenu?.cards[2]?.card?.card?.info?.cuisines.join(", ")}</h2>
+      <h1>{name}</h1>
+      <h2>{avgRating}</h2>
+      <h3>{cuisines.join(", ")}</h3>
       <div>
         Menu
         <ul>item 1</ul>
