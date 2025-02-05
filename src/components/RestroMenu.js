f955d5a55bc2f22b374ae142629632deee6c7198
Siddhant Swain <siddhantswain11@gmail.com>
2025-02-05 11:57:02 +0530

    Displayed the menu dynamically using api data(Recommended section)

--- src/components/RestroMenu.js
+++ src/components/RestroMenu.js
@@ -2,29 +2,37 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer.js";
 
 const RestroMenu = () => {
-  
   const [resMenu, setResMenu] = useState(null);
-
+  const [menuItems, setMenuItems] = useState(null);
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.8134554&lng=85.8312359&restaurantId=702417&catalog_qa=undefined&submitAction=ENTER");
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.8134554&lng=85.8312359&restaurantId=702417&catalog_qa=undefined&submitAction=ENTER"
+    );
     const json = await data.json();
     //console.log(json);
     setResMenu(json.data?.cards[2]?.card?.card?.info);
-    console.log(json.data?.cards[2]?.card?.card?.info)
-    
+    console.log(json);
+    setMenuItems(
+      json.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+        ?.card?.itemCards
+    );
+    console.log(
+      json.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+        ?.card?.itemCards
+    );
   };
-  
-  if (resMenu === null) return<Shimmer />;
-  
+
+  if (resMenu === null) return <Shimmer />;
+
   const { name, avgRating, cuisines } = resMenu;
   console.log("qwe ", cuisines);
+  const {} = menuItems;
 
- // return <h3>{JSON.stringify(cuisines)}</h3>;
+  // return <h3>{JSON.stringify(cuisines)}</h3>;
   return (
     <div>
       <h1>{name}</h1>
@@ -32,9 +40,14 @@ const RestroMenu = () => {
       <h3>{cuisines.join(", ")}</h3>
       <div>
         Menu
-        <ul>item 1</ul>
-        <ul>item 2</ul>
-        <ul>item 3</ul>
+        <ul>
+          {menuItems.map((item) => (
+            <li>
+              {item?.card?.info?.name}-{" Rs."}
+              {item?.card?.info?.price / 100}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
