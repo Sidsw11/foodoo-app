e3503cfc1abda0ec23b6455f0b8ef67334d8e046
Siddhant Swain <siddhantswain11@gmail.com>
2025-02-05 12:08:01 +0530

    added menu images to the menu list

--- src/components/RestroMenu.js
+++ src/components/RestroMenu.js
@@ -44,7 +44,13 @@ const RestroMenu = () => {
           {menuItems.map((item) => (
             <li>
               {item?.card?.info?.name}-{" Rs."}
-              {item?.card?.info?.price / 100}
+              {item?.card?.info?.price / 100} <img
+        className="MenuPic"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          item?.card?.info?.imageId
+        }
+      ></img>
             </li>
           ))}
         </ul>
